c0327616cc13393e9f0b19c678cd3cf48c3cc5cc
Luke Lancaster <luketlancaster@gmail.com>
2020-05-26 21:02:18 -0500

    add to cart and actually print it

--- javascripts/components/cart.js
+++ javascripts/components/cart.js
@@ -1,12 +1,22 @@
 import utils from '../helpers/utils.js';
+import cartData from '../helpers/data/cartData.js';
+
+const addToCart = (newBook) => {
+  cartData.setCart(newBook);
+  cartToDom();
+};
 
 const cartToDom = () => {
-  const domString = `
-    <h3>THIS IS THE CART</h3>
-  `;
+  const myCart = cartData.getCart();
+  let domString = '';
+
+  for (let i = 0; i < myCart.length; i++) {
+    const cartItem = myCart[i];
+    domString += `<h3>${cartItem.title}</h3>`;
+  }
 
   utils.printToDom('#cart', domString);
 };
 
 
-export default { cartToDom };
+export default { addToCart };
